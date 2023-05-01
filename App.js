ca81e972ef3ac0fa160615c69da49fc63f099a72
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-05-01 19:54:39 +0200

    Removed unnecessary styling

--- App.js
+++ App.js
@@ -31,17 +31,16 @@ const App = () => {
         </Stack.Navigator>
       </NavigationContainer>
     
-
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
 
 export default App;
